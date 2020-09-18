5057959ebe7b9caecfffa24a6c3231129e8de257
Thorsten Müller <thorsten.mueller@js-soft.com>
2020-09-18 15:35:01 +0200

    chore: added Northwind service connectivity

--- srv/cbo-service.cds
+++ srv/cbo-service.cds
@@ -1,9 +1,13 @@
 using {tbusinessobject as external} from './external/tbusinessobject.csn';
+using {NorthWind as externNorthwind} from './external/NorthWind';
 using {db as dbtest} from '../db/schema';
 
 
 @path: '/browse'
 service browse {
     entity People as projection on dbtest.People;
+    entity Products as projection on externNorthwind.Products {
+        key ID, Name, Description, ReleaseDate, DiscontinuedDate, Rating, Price
+    };
     entity TBusinessObj as projection on external.xJSSxTBUSINESSOBJECT ;
 }
--- srv/cbo-service.js
+++ srv/cbo-service.js
@@ -1,9 +1,16 @@
 const cds = require('@sap/cds')
 
 module.exports = cds.service.impl(async function() {
-	const { TBusinessObj } = this.entities;
-	const service = await cds.connect.to('tbusinessobject');
-	this.on('READ', TBusinessObj, request => {
-		return service.tx(request).run(request.query);
-	});
+    const { TBusinessObj, Products } = this.entities;
+    // console.log(this.entities)
+    const s9d = await cds.connect.to('tbusinessobject');
+    const northwind = await cds.connect.to('NorthWind')
+    
+    this.on('READ', TBusinessObj, request => {
+		return s9d.tx(request).run(request.query);
+    });
+    
+	this.on('READ', Products, request => {
+		return northwind.tx(request).run(request.query);
+    });
 });

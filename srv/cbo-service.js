490fd59095afc71900db781d17c0626528eed472
Thorsten Müller <thorsten.mueller@js-soft.com>
2020-10-23 11:22:27 +0200

    chore: add manual convertion for $count property

--- srv/cbo-service.js
+++ srv/cbo-service.js
@@ -1,20 +1,30 @@
-const cds = require('@sap/cds');
+const cds = require("@sap/cds");
+const { response } = require("express");
 
-module.exports = cds.service.impl(async function() {
-    const s9d_tbusobj = await cds.connect.to('xJSSxTBUSINESSOBJECT');
-    const s9d_peopleSet = await cds.connect.to("s9d_Z_PLT_SRV")
-    const {xJSSxTBUSINESSOBJECT, PeopleSet} = this.entities;
-    
+module.exports = cds.service.impl(async function () {
+  const s9d_tbusobj = await cds.connect.to("xJSSxTBUSINESSOBJECT");
+  const s9d_peopleSet = await cds.connect.to("s9d_Z_PLT_SRV");
+  const { xJSSxTBUSINESSOBJECT, PeopleSet } = this.entities;
 
-    this.on('READ', xJSSxTBUSINESSOBJECT, async (request) => {
-      let response=  await s9d_tbusobj.tx(request).run(request.query);
-      return response
+  this.on("READ", xJSSxTBUSINESSOBJECT, async (request) => {
+    let response = await s9d_tbusobj.tx(request).run(request.query);
+    return response;
+  });
+  this.on("READ", PeopleSet, async (request) => {
+    let response = await s9d_peopleSet.tx(request).run(request.query);
+
+    //convert response between OData v2 and v4
+    response.$count = response.length
+    response.forEach((element) => {
+      element.Birthday = convertDate(element.Birthday)
     });
-    this.on('READ', PeopleSet, async (request) => {
-      let response=  await s9d_peopleSet.tx(request).run(request.query);
-      response.forEach(element => {
-        element.Birthday = new Date(1 * element.Birthday.replace(/\/Date\(([-\d]*)\)\//, "$1")).toISOString()//.split('T')[0]
-      });
-      return response
-    });
+    return response;
+  });
+
+  function countResponse(response) {
+    return response.length
+  }
+  function convertDate (dateString) {
+    return new Date(1 * dateString.replace(/\/Date\(([-\d]*)\)\//, "$1")).toISOString()
+  }
 });

21c088c7039638222692993860cb638f342dc722
Thorsten Müller <thorsten.mueller@js-soft.com>
2020-12-01 16:19:52 +0100

    chore: remove private properties

--- srv/cbo-service.cds
+++ srv/cbo-service.cds
@@ -1,13 +1,9 @@
-using {xJSSxTBUSINESSOBJECT as externalxJSSx} from './external/xJSSxTBUSINESSOBJECT.csn';
-using {s9d_Z_PLT_SRV as s9dEmployee} from './external/s9d_Z_PLT_SRV';
+
 using {NorthWind as externNorthwind} from './external/NorthWind';
 
 
 @path: '/browse'
 service browse {
-    entity xJSSxTBUSINESSOBJECT as projection on externalxJSSx.xJSSxTBUSINESSOBJECT;
-    entity PeopleSet as select from s9dEmployee.PeopleSet;
-    @readonly
     entity NorthWind as select from externNorthwind.Products{
         key ID, Name, Description, ReleaseDate, DiscontinuedDate, Rating, Price
     };
--- srv/cbo-service.js
+++ srv/cbo-service.js
@@ -2,35 +2,13 @@ const cds = require("@sap/cds");
 const { response } = require("express");
 
 module.exports = cds.service.impl(async function () {
-  const s9d_tbusobj = await cds.connect.to("xJSSxTBUSINESSOBJECT");
-  const s9d_peopleSet = await cds.connect.to("s9d_Z_PLT_SRV");
   const cnt_NorthWind = await cds.connect.to("NorthWind")
 
-  const { xJSSxTBUSINESSOBJECT, PeopleSet, NorthWind } = this.entities;
-
-  this.on("READ", xJSSxTBUSINESSOBJECT, async (request) => {
-    let response = await s9d_tbusobj.tx(request).run(request.query);
-    return response;
-  });
-  this.on("READ", PeopleSet, async (request) => {
-    let response = await s9d_peopleSet.tx(request).run(request.query);
-
-    //convert response between OData v2 and v4
-    response.$count = response.length
-    response.forEach((element) => {
-      element.Birthday = convertDate(element.Birthday)
-    });
-    return response;
-  });
+  const { NorthWind } = this.entities;
   this.on("READ", NorthWind, async (request) => {
     let response = await cnt_NorthWind.tx(request).run(request.query);
     return response;
   });
 
-  function countResponse(response) {
-    return response.length
-  }
-  function convertDate (dateString) {
-    return new Date(1 * dateString.replace(/\/Date\(([-\d]*)\)\//, "$1")).toISOString()
-  }
+
 });
